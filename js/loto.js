41c80d348290b6e113911fa28ab9c9971182a4db
Raphaël MARQUES <work.rmarques@gmail.com>
2014-02-07 11:48:14 +0100

    Improved algo

--- js/loto.js
+++ js/loto.js
@@ -5,7 +5,49 @@ function lancerLoto() {
     afficherMontantsGagnes(nombres);
 }
 
+////////////////////////////////////////////////////////////
+
+var MIN_NUM = 1;
+var MAX_NUM = 49;
+var MAX_COMPL = 10;
+var NUM_LOTO = 5;
+var lotonum = initialiserLoto(MAX_NUM);
 function getLoto() {
+    var nombres = lotonum.slice();
+    var loto = [];
+
+    var pickedNombre;
+    for(var i = MIN_NUM; i <= NUM_LOTO; ++i) {
+        pickedNombre = Math.ceil(Math.random() * MAX_NUM) + 1;
+
+        var check = nombres[pickedNombre];
+        if(check) {
+            loto[i] = check;
+        } else {
+            --i;
+            continue;
+        }
+        nombres.splice(pickedNombre, 1);
+    }
+
+    nombres = initialiserLoto(MAX_COMPL);
+    loto[NUM_LOTO + 1] = nombres[parseInt(Math.random() * MAX_COMPL) + 1];
+
+    return loto;
+}
+
+function initialiserLoto (max) {
+    var grille = [];
+    for(var i = MIN_NUM; i <= max; ++i) {
+        grille[i] = i;
+    }
+    return grille;
+}
+
+
+////////////////////////////////////////////////////////////
+
+function getLoto2() {
     var nombres = [];
     for(var i = 0 ; i < 5; i++){
         nombres[i] = leNumeroQuiSort(nombres);
@@ -16,7 +58,7 @@ function getLoto() {
 
 function afficherResultats(nombres) {
     for(var i = 0; i < nombres.length; i++){
-        $('#res'+i).html(nombres[i]);
+        $('#res'+i).html(nombres[i+1]);
     }
 }
 
